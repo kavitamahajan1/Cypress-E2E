8226b2494bc4fd8c97aadc395f59721a5a33c6c3
KAVITA\kavit <kavita.mahajan@nvest.in>
2023-11-06 18:15:48 +0530

    Yaml file changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     watchForFileChanges: false,
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 10000,    
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
   env:{
